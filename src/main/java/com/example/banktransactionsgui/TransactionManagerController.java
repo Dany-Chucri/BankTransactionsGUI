6f61177cfc5ac351977ea1755826f226567a4c58
Dany-Chucri <chucridany@gmail.com>
2023-11-04 19:08:57 -0400

    Added instance variables for necessary nodes corresponding to their fx:id

--- src/main/java/com/example/banktransactionsgui/TransactionManagerController.java
+++ src/main/java/com/example/banktransactionsgui/TransactionManagerController.java
@@ -1,14 +1,36 @@
 package com.example.banktransactionsgui;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class TransactionManagerController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private TabPane mainTabPane;
+
+    @FXML
+    private Tab openClose, depositWithdraw, accountDatabase;
+
+    @FXML
+    private TextArea textArea;
+
+    @FXML
+    private ToggleGroup AccountType1, AccountType2, Campus;
+
+    @FXML
+    private TextField firstName1, lastName1, initialDeposit, firstName2, lastName2, amount;
+
+    @FXML
+    private DatePicker dob1, dob2;
+
+    @FXML
+    private Button open, close, clear, deposit, withdraw, printAll, loadAccounts, printFeesInterest, updatePrint;
+
+    @FXML
+    private RadioButton checking1, checking2, savings1, savings2, collegeChecking1, collegeChecking2,
+        moneyMarket1, moneyMarket2, camden, nb, newark;
+
+    @FXML
+    private CheckBox loyalty;
+
 }
