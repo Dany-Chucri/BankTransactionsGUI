7aaac1b8fbbe59707c59b45e0768aabf495d0384
madhurn1 <madhurnutulapati@gmail.com>
2023-11-04 20:33:26 -0400

    nothin

--- src/main/java/com/example/banktransactionsgui/TransactionManagerMain.java
+++ src/main/java/com/example/banktransactionsgui/TransactionManagerMain.java
@@ -16,7 +16,6 @@ public class TransactionManagerMain extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
     public static void main(String[] args) {
         launch();
     }
