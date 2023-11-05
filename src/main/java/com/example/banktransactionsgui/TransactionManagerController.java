978441fe1a9b2fbcff9f4bdec5511ea964ea8697
Dany-Chucri <chucridany@gmail.com>
2023-11-04 20:09:38 -0400

    Added sample event handlers and implemented simple clear handlers

--- src/main/java/com/example/banktransactionsgui/TransactionManagerController.java
+++ src/main/java/com/example/banktransactionsgui/TransactionManagerController.java
@@ -1,5 +1,6 @@
 package com.example.banktransactionsgui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
@@ -24,7 +25,7 @@ public class TransactionManagerController {
     private DatePicker dob1, dob2;
 
     @FXML
-    private Button open, close, clear, deposit, withdraw, printAll, loadAccounts, printFeesInterest, updatePrint;
+    private Button open, close, clear, deposit, withdraw, printAll, loadAccounts, printFeesInterests, printUpdate;
 
     @FXML
     private RadioButton checking1, checking2, savings1, savings2, collegeChecking1, collegeChecking2,
@@ -33,4 +34,86 @@ public class TransactionManagerController {
     @FXML
     private CheckBox loyalty;
 
+    @FXML
+    void clearFirst(ActionEvent event) {
+        firstName1.clear();
+        lastName1.clear();
+        dob1.getEditor().clear();
+        initialDeposit.clear();
+        checking1.setSelected(false);
+        collegeChecking1.setSelected(false);
+        savings1.setSelected(false);
+        moneyMarket1.setSelected(false);
+        camden.setDisable(true);
+        nb.setDisable(true);
+        newark.setDisable(true);
+        loyalty.setDisable(true);
+    }
+
+    @FXML
+    void clearSecond(ActionEvent event) {
+        firstName2.clear();
+        lastName2.clear();
+        dob2.getEditor().clear();
+        amount.clear();
+        checking2.setSelected(false);
+        collegeChecking2.setSelected(false);
+        savings2.setSelected(false);
+        moneyMarket2.setSelected(false);
+    }
+
+    @FXML
+    void dateValidity(ActionEvent event) {
+
+    }
+
+    @FXML
+    void enableLoyalty(ActionEvent event) {
+
+    }
+
+    @FXML
+    void enableCampus(ActionEvent event) {
+
+    }
+
+    @FXML
+    void open(ActionEvent event) {
+
+    }
+
+    @FXML
+    void close(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deposit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void withdraw(ActionEvent event) {
+
+    }
+
+    @FXML
+    void printSorted(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loadAccountsFromFile(ActionEvent event) {
+
+    }
+
+    @FXML
+    void printUpdatedBalances(ActionEvent event) {
+
+    }
+
+    @FXML
+    void printFeesAndInterests(ActionEvent event) {
+
+    }
 }
