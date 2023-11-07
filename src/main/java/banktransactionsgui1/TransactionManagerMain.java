04d1da60d0a8ce1a084dd44a3f35fe53ce39a9b3
Dany-Chucri <chucridany@gmail.com>
2023-11-06 19:31:33 -0500

    Resolution alterations

--- src/main/java/banktransactionsgui1/TransactionManagerMain.java
+++ src/main/java/banktransactionsgui1/TransactionManagerMain.java
@@ -11,7 +11,7 @@ public class TransactionManagerMain extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(TransactionManagerMain.class.getResource("TransactionManagerView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 720, 960);
+        Scene scene = new Scene(fxmlLoader.load(), 720, 800);
         stage.setTitle("Transaction Manager");
         stage.setScene(scene);
         stage.show();
